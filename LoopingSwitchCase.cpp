7f8fea2dcf8843b5ae05ab1b3b3deee647d84a41
ArgyTataMaulana <halmaheru37@gmail.com>
2025-03-18 20:15:06 +0700

    add perulangan for pencacahan turun

--- LoopingSwitchCase.cpp
+++ LoopingSwitchCase.cpp
@@ -19,4 +19,13 @@ for (n = 0; n < 5; n++)
 }
 cout << "Nilai n terakhir = " << n << endl;
 cout << endl;
+
+// Perulangan dengan for
+for (n = 5; n > 0; n--)
+{
+    cout << "Nilai n = " << n << "Selamat Datang" << endl;
+}
+
+cout << "Nilai n Terakhir = " << n;
+cout << endl;
 }
